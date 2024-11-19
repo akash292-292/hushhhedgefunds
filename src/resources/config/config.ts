419b84b861f1600ab01a1deb19a4bc57ee92b0df
Apoorv Bedmutha <i-apoorv@hushh.ai>
2024-11-19 15:15:58 +0530

    FIxed redirect URL

--- src/resources/config/config.ts
+++ src/resources/config/config.ts
@@ -4,7 +4,7 @@ const config = {
   SUPABASE_URL: "https://rpmzykoxqnbozgdoqbpc.supabase.co",
   SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwbXp5a294cW5ib3pnZG9xYnBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5Mjc5NzEsImV4cCI6MjAxNzUwMzk3MX0.3GwG8YQKwZSWfGgTBEEA47YZAZ-Nr4HiirYPWiZtpZ0",
-  redirect_url: "http://localhost:5173",
+  redirect_url: "https://www.hushhtech.com/",
 };
 
 function createSupabaseClient() {
