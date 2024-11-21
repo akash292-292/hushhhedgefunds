c939986115c00d941136c184a88c38919715e8de
Apoorv Bedmutha <i-apoorv@hushh.ai>
2024-11-21 20:16:33 +0530

    implemented apple sign in

--- src/resources/config/config.ts
+++ src/resources/config/config.ts
@@ -1,10 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
-
+import featureFlags from "./featureFlags";
 const config = {
   SUPABASE_URL: "https://rpmzykoxqnbozgdoqbpc.supabase.co",
   SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwbXp5a294cW5ib3pnZG9xYnBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5Mjc5NzEsImV4cCI6MjAxNzUwMzk3MX0.3GwG8YQKwZSWfGgTBEEA47YZAZ-Nr4HiirYPWiZtpZ0",
   redirect_url: "https://www.hushhtech.com/",
+  featureFlags: featureFlags,
 };
 
 function createSupabaseClient() {
--- src/services/authentication/authentication.ts
+++ src/services/authentication/authentication.ts
@@ -6,6 +6,7 @@ import isLoggedIn from "./isLoggedIn";
 import getFullName from "./getFullName";
 import getSession from "./getSession";
 import emailLogin from "./emailLogin";
+import appleSignIn from "./appleSignIn";
 const authentication = {
   googleSignIn: googleSignIn,
   getUserDetails: getUserDetails,
@@ -14,6 +15,7 @@ const authentication = {
   isLoggedIn: isLoggedIn,
   getFullName: getFullName,
   getSession: getSession,
-  emailLogin: emailLogin
+  emailLogin: emailLogin,
+  appleSignIn: appleSignIn
 };
 export default authentication;
